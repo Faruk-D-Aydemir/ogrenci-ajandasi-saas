42fb09c87be5d6a7c0fe7d07f3a191667c3791d8
Faruk-D-Aydemir <muhammedfarukaydemir@gmail.com>
2025-10-25 01:11:48 +0300

    build.sh python komutu mutlak yol ile guncellendi (Exited 1 hatasi cozumu).

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 # 1. Gerekli kütüphaneleri kur
 pip install -r requirements.txt
 
-# 2. Veritabanı tablolarını oluştur (db.create_all() komutunu çalıştır)
-python -c "from app import app, db; with app.app_context(): db.create_all()"
+# 2. Veritabanı tablolarını oluştur (Mutlak Python yolu ile zorla çalıştırma)
+/usr/bin/env python -c "from app import app, db; with app.app_context(): db.create_all()"
